91afa99fc0357ed217654d49402ea89f8bddbf48
KsuSh7 <shevchuk.kseniia1@student.uzhnu.edu.ua>
2025-04-26 21:02:27 +0300

    змінила базу даних

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_jwt_extended import JWTManager
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Duplik64!@localhost:5432/travelplanner'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'Dupl!k64'
 jwt = JWTManager(app)
